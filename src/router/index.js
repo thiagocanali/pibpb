7b8b4bb8d992b7f58c9daaf2faa318a8766d53be
Thiago Schwartz <67984677+thiagocanali@users.noreply.github.com>
2025-12-13 20:51:24 -0300

    fix: use hash history for GitHub Pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import routes from './routes';
+import { createRouter, createWebHashHistory } from 'vue-router'
+import routes from './routes'
 
-const router = Router();
-export default router;
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
 
-function Router() {
-  const router = new createRouter({
-    history: createWebHistory(),
-    routes
-  });
-  return router;
-}
+export default router
